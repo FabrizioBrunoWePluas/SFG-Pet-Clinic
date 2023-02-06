6a6ceacbb89e1bf5816c4949bc13e8d2cf63521c
FabrizioBrunoWePlus <fabrizio.bruno@we-plus.eu>
2023-02-06 16:57:31 +0100

    Adding Vet index and controller

--- pet-clinic-web/src/main/java/UAccademy/Controller/VetController.java
+++ pet-clinic-web/src/main/java/UAccademy/Controller/VetController.java
@@ -1,4 +1,14 @@
 package UAccademy.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class VetController {
+
+    @RequestMapping({"/vet","/vet/index","/vet/index.html"})
+    public String listVet(){
+        return "vet/VetIndex";
+    }
+
 }
