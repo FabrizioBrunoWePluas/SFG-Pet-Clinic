7b1db3731990ee77d3ebd04800c40e1296cd61c7
FabrizioBrunoWePlus <fabrizio.bruno@we-plus.eu>
2023-02-07 17:53:21 +0100

    Added proprieties

--- pet-clinic-data/src/main/java/UAccademy/Model/Owner.java
+++ pet-clinic-data/src/main/java/UAccademy/Model/Owner.java
@@ -1,8 +1,18 @@
 package UAccademy.Model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Set;
 
+@Getter
+@Setter
 public class Owner extends Person{
+
+    private String address;
+    private String city;
+    private String telephone;
+
 
     private Set<Pet> pets;
 
