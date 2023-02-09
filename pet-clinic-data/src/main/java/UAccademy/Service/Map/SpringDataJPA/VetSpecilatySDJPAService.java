f6c646268bc693a5ecf741f715459220756c032d
FabrizioBrunoWePlus <fabrizio.bruno@we-plus.eu>
2023-02-09 10:49:26 +0100

    Added Sprind Data JPA Service

--- pet-clinic-data/src/main/java/UAccademy/Service/Map/SpringDataJPA/VetSpecilatySDJPAService.java
+++ pet-clinic-data/src/main/java/UAccademy/Service/Map/SpringDataJPA/VetSpecilatySDJPAService.java
@@ -4,10 +4,15 @@ import UAccademy.Model.Pet;
 import UAccademy.Model.Speciality;
 import UAccademy.Repository.SpecialtyRepository;
 import UAccademy.Service.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("SpringDataJPA")
 public class VetSpecilatySDJPAService implements SpecialityService{
 
     private final SpecialtyRepository specialtyRepository;
