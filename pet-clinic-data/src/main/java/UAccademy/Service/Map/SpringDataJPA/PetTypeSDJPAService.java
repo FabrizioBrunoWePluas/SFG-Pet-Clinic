7dba7ff4d39c6db303efa0b459d7c29f4b8147fd
FabrizioBrunoWePlus <fabrizio.bruno@we-plus.eu>
2023-02-09 10:41:47 +0100

    Added Sprind Data JPA Service

---
+++ pet-clinic-data/src/main/java/UAccademy/Service/Map/SpringDataJPA/PetTypeSDJPAService.java
@@ -0,0 +1,54 @@
+package UAccademy.Service.Map.SpringDataJPA;
+
+import UAccademy.Model.PetType;
+import UAccademy.Model.Vet;
+import UAccademy.Repository.PetTypeRepository;
+import UAccademy.Service.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("SpringDataJPA")
+public class PetTypeSDJPAService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJPAService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> pet = new HashSet<>();
+
+        //The double colon (::) operator, also known as method reference operator in Java, is used to call a method by referring to it with the help of its class directly.
+        // They behave exactly as the lambda expressions. The only difference it has from lambda expressions is that this uses direct
+        // reference to the method by name instead of providing a delegate to the method.
+        petTypeRepository.findAll().forEach(pet::add);
+
+        return pet;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType objcet) {
+        return petTypeRepository.save(objcet);
+    }
+
+    @Override
+    public void delete(PetType objcet) {
+        petTypeRepository.delete(objcet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
